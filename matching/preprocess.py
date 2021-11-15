b442c3729c9152032623ce03f80cd2be03cae1ea
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-11-15 14:50:25 +0000

    Add EPC preprocessing functions (#1)
    * Add EPC preprocessing functions
    
    * Ignore pandas for mypy

---
+++ matching/preprocess.py
@@ -0,0 +1,55 @@
+import re
+from typing import Optional
+
+import pandas as pd
+
+
+def extract_building_name(address_line: Optional[str]) -> Optional[str]:
+    if address_line is None or "flat" in address_line:
+        return None
+
+    if match := re.match(r"[a-z\s]+", address_line):
+        return match.group()
+
+    return None
+
+
+def extract_building_number(address_line: Optional[str]) -> Optional[str]:
+    if address_line is None:
+        return None
+
+    if match := re.match(r"[0-9]+[a-z]*", address_line):
+        return match.group()
+
+    return None
+
+
+def extract_flat_number(address_line: str) -> Optional[str]:
+    if match := re.match(r"flat ([a-z0-9]+)", address_line):
+        return match.group(1)
+
+    if match := re.match(r"([\w\s]+)(?=\sflat)", address_line):
+        return match.group(1)
+
+    return None
+
+
+def casefold_epc_addresses(addresses: pd.DataFrame) -> pd.DataFrame:
+    for col in ["address_line_1", "address_line_2", "address_line_3"]:
+        addresses[col] = addresses[col].str.casefold()
+    return addresses
+
+
+def add_epc_features(addresses: pd.DataFrame) -> pd.DataFrame:
+    building_number_address_line_1 = addresses["address_line_1"].map(
+        extract_building_number
+    )
+    building_number_address_line_2 = addresses["address_line_2"].map(
+        extract_building_number
+    )
+    addresses["building_number"] = building_number_address_line_1.combine_first(
+        building_number_address_line_2
+    )
+    addresses["flat_number"] = addresses["address_line_1"].map(extract_flat_number)
+    addresses["building_name"] = addresses["address_line_1"].map(extract_building_name)
+    return addresses
---
+++ matching/tests/test_preprocess.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+from matching.preprocess import (
+    add_epc_features,
+    casefold_epc_addresses,
+    extract_building_name,
+    extract_building_number,
+    extract_flat_number,
+)
+
+
+def test_extract_building_name() -> None:
+    assert extract_building_name(None) is None
+    assert extract_building_name("wayfarer") == "wayfarer"
+    assert extract_building_name("grey house") == "grey house"
+    assert extract_building_name("flat a") is None
+    assert extract_building_name("32 rochdale road") is None
+    assert extract_building_name("32, rochdale road") is None
+
+
+def test_extract_building_number() -> None:
+    assert extract_building_number(None) is None
+    assert extract_building_number("8 south road") == "8"
+    assert extract_building_number("23, halifax road") == "23"
+    assert extract_building_number("8c, letchmore road") == "8c"
+    assert extract_building_number("18c meldon road") == "18c"
+    assert extract_building_number("flat a") is None
+    assert extract_building_number("flat 3") is None
+
+
+def test_extract_flat_number() -> None:
+    assert extract_flat_number("flat 15") == "15"
+    assert extract_flat_number("flat 7") == "7"
+    assert extract_flat_number("flat 1 queens court") == "1"
+    assert extract_flat_number("flat a") == "a"
+    assert extract_flat_number("basement flat") == "basement"
+    assert extract_flat_number("first floor flat") == "first floor"
+    assert extract_flat_number("fancy building name") is None
+
+
+def test_casefold_epc_addresses() -> None:
+    epc_addresses = pd.DataFrame(
+        [
+            {
+                "address_line_1": "First Floor Flat",
+                "address_line_2": "1, London Road",
+                "address_line_3": "Marsden Moor",
+            }
+        ]
+    )
+
+    got = epc_addresses.pipe(casefold_epc_addresses)
+
+    want = pd.DataFrame(
+        [
+            {
+                "address_line_1": "first floor flat",
+                "address_line_2": "1, london road",
+                "address_line_3": "marsden moor",
+            }
+        ]
+    )
+
+    pd.testing.assert_frame_equal(got, want)
+
+
+def test_add_epc_features() -> None:
+    tests = [
+        (
+            {
+                "address_line_1": "flat c",
+                "address_line_2": "97, grove road",
+            },
+            {"building_number": "97", "flat_number": "c"},
+        ),
+        (
+            {
+                "address_line_1": "32, pilkington road",
+                "address_line_2": None,
+            },
+            {"building_number": "32"},
+        ),
+        (
+            {
+                "address_line_1": "the mansion",
+                "address_line_2": "1, garden road",
+            },
+            {"building_name": "the mansion", "building_number": "1"},
+        ),
+    ]
+
+    addresses, features = zip(*tests)
+    got = pd.DataFrame(addresses).pipe(add_epc_features)[
+        ["building_number", "flat_number", "building_name"]
+    ]
+    want = pd.DataFrame(features)
+    pd.testing.assert_frame_equal(got, want)
