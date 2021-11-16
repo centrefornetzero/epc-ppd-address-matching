f50b23fd8cc6ed569ddc4af0613331f1a6d557ae
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-11-16 09:56:46 +0000

    Write to separate folders
    So pd.read_parquet can load them all.

--- scripts/extract_from_bigquery.sh
+++ scripts/extract_from_bigquery.sh
@@ -10,7 +10,8 @@ bq extract --location EU --destination_format PARQUET \
     "$GCP_PROJECT_ID:prod_epc_ppd_address_matching.dim_ppd_addresses" \
     "gs://$SCRATCH_BUCKET_NAME/$USER/dim_ppd_addresses-*.parquet"
 
-mkdir -p data/addresses
+mkdir -p data/epc_addresses
+mkdir -p data/ppd_addresses
 
-gsutil cp "gs://$SCRATCH_BUCKET_NAME/$USER/dim_epc_addresses-*.parquet" data/addresses/
-gsutil cp "gs://$SCRATCH_BUCKET_NAME/$USER/dim_ppd_addresses-*.parquet" data/addresses/
+gsutil cp "gs://$SCRATCH_BUCKET_NAME/$USER/dim_epc_addresses-*.parquet" data/epc_addresses/
+gsutil cp "gs://$SCRATCH_BUCKET_NAME/$USER/dim_ppd_addresses-*.parquet" data/ppd_addresses/
