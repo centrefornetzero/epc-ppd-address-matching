ecd9309b2ecd28f3a98ccbf38daaec7b3f817da8
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-11-18 10:32:51 +0000

    Use unweighted graph
    Had chosen weighted because prototype originally found the maximal weight
    matching.

--- matching/matches.py
+++ matching/matches.py
@@ -10,10 +10,10 @@ def get_matches(
     min_similarity = candidate_matches["_score"] >= min_score
     edges = (
         tuple(edge)
-        for edge in candidate_matches[min_similarity][
-            ["epc_id", "ppd_id", "_score"]
-        ].itertuples(index=False)
+        for edge in candidate_matches[min_similarity][["epc_id", "ppd_id"]].itertuples(
+            index=False
+        )
     )
     similarity_graph = nx.Graph()
-    similarity_graph.add_weighted_edges_from(edges)
+    similarity_graph.add_edges_from(edges)
     yield from nx.connected_components(similarity_graph)
