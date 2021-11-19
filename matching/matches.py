b4ad12c5184642df5f189f08c3c7bfacb0470ddb
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-11-19 14:09:17 +0000

    Use view instead of copy when adding edges
    Reduces peak memory usage by ~40%. On all postcodes starting with E, reduced
    from ~5750MB to ~3500MB.

--- matching/matches.py
+++ matching/matches.py
@@ -8,11 +8,9 @@ def get_matches(
     candidate_matches: pd.DataFrame, min_score: float
 ) -> Iterator[Set[str]]:
     min_similarity = candidate_matches["_score"] >= min_score
-    edges = (
-        tuple(edge)
-        for edge in candidate_matches[min_similarity][["epc_id", "ppd_id"]].itertuples(
-            index=False
-        )
+    edges = zip(
+        candidate_matches.loc[min_similarity, "epc_id"],
+        candidate_matches.loc[min_similarity, "ppd_id"],
     )
     similarity_graph = nx.Graph()
     similarity_graph.add_edges_from(edges)
