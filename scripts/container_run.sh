4ee59dc472fe229b8e9ec86086d384c71c1a539b
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-12-03 13:45:31 +0000

    Run match job on GCP (#13)
    * Run script in container
    
    * Install gcloud SDK in container
    
    * Specify --project_id arg for bq
    
    Failing with a strange project name. I specify default in my
    ~/.bigqueryrc file, so setting here.
    
    * Add missing parallel dependency
    
    * Erase bucket destination before extract
    
    Otherwise if the job is run twice (before the bucket is automatically
    erased) there might be files left over from the last run.
    
    * Remove prefix
    
    Datetime stamp will be in file metadata anyway.
    Git not installed in container.
    
    * Write matches to parent directory
    
    Everything else already is. No need for a matches directory.
    
    * Add .vscode to gitignore
    
    * Add submit job to workflow
    
    * Set project ID for setup-glcoud
    
    * Add missing IMAGE_URI
    
    * Hardcode repo name
    
    Not easy to get it via a Github Action expression.
    Will break if we re-name repo but unlikely to happen.
    
    * Run match job on main only

---
+++ scripts/container_run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eu
+
+echo "==> Erasing bucket working directory..."
+
+gsutil rm "gs://$SCRATCH_BUCKET_NAME/$USER/*"
+
+echo "==> Starting BQ extract jobs..."
+
+bq extract --project_id "$GCP_PROJECT_ID" --location EU --destination_format PARQUET \
+    "$GCP_PROJECT_ID:prod_epc_ppd_address_matching.dim_epc_addresses" \
+    "gs://$SCRATCH_BUCKET_NAME/$USER/dim_epc_addresses-*.parquet"
+
+bq extract --project_id "$GCP_PROJECT_ID" --location EU --destination_format PARQUET \
+    "$GCP_PROJECT_ID:prod_epc_ppd_address_matching.dim_ppd_addresses" \
+    "gs://$SCRATCH_BUCKET_NAME/$USER/dim_ppd_addresses-*.parquet"
+
+echo "==> Downloading extracts from GCS..."
+
+mkdir -p data/epc_addresses
+mkdir -p data/ppd_addresses
+
+gsutil cp "gs://$SCRATCH_BUCKET_NAME/$USER/dim_epc_addresses-*.parquet" data/epc_addresses/
+gsutil cp "gs://$SCRATCH_BUCKET_NAME/$USER/dim_ppd_addresses-*.parquet" data/ppd_addresses/
+
+echo "==> Partitioning data..."
+
+python -m matching.partition data/epc_addresses data/partitioned_epc_addresses
+python -m matching.partition data/ppd_addresses data/partitioned_ppd_addresses
+
+echo "==> Matching records..."
+
+mkdir -p data/matches
+
+parallel --link --tagstring "Job {#}" \
+    python -m matching --cluster-prefix "{#}-" {} "data/matches/matches-{#}.parquet" \
+    ::: "$(ls -d data/partitioned_epc_addresses/**)" \
+    ::: "$(ls -d data/partitioned_ppd_addresses/**)"
+
+echo "==> Uploading to GCS..."
+
+gsutil cp data/matches/matches-*.parquet "gs://$SCRATCH_BUCKET_NAME/$USER/"
