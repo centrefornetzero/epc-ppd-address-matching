3154340daabb152fd4c4f8018eefc6b20b55d616
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-12-03 12:13:45 +0000

    Add submit job to workflow

---
+++ scripts/submit_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat > config.yml <<EOF
+workerPoolSpecs:
+  machineSpec:
+    machineType: e2-standard-16
+  replicaCount: 1
+  containerSpec:
+    imageUri: $IMAGE_URI
+    env:
+     - name: GCP_PROJECT_ID
+       value: $GCP_PROJECT_ID
+     - name: SCRATCH_BUCKET_NAME
+       value: $SCRATCH_BUCKET_NAME
+     - name: USER
+       value: epc-ppd-matching
+serviceAccount: $SERVICE_ACCOUNT
+EOF
+
+gcloud ai custom-jobs create \
+    --region europe-west2 \
+    --display-name epc-ppd-matching-test-job \
+    --config config.yml
